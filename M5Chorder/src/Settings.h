27e2bf8e3257865f41bb8beb0980c69954a6aff5
wararyo <wararyo@gmail.com>
2020-05-17 15:33:44 +0900

    serialization of std::vector

--- M5Chorder/src/Settings.h
+++ M5Chorder/src/Settings.h
@@ -45,38 +45,6 @@ template <class T>
 class is_serializable_externally :
     public decltype(is_serializable_externally_impl::check<T>(std::declval<const char *>(),std::declval<T>(),std::declval<OutputArchive>())){};
 
-template <class T, typename std::enable_if<is_serializable<T>::value, std::nullptr_t>::type = nullptr>
-class SettingItem {
-public:
-    const char *name;
-    std::vector<SettingItem*> children;
-    T content;
-    SettingItem(const char *name,T content) {
-        this->name = name;
-        this->content = content;
-    }
-    SettingItem(const char *name,std::vector<SettingItem*> children){
-        this->name = name;
-        this->children = children;
-    }
-    void serialize(OutputArchive &archive,const char *key) {
-        if(children.empty()) {
-            Serial.printf("SettingItem#serialize(%s,content)",name);
-            archive(name,content);
-        } else {
-            Serial.printf("SettingItem#serialize(%s,children)",name);
-            // archive(name,children);
-        }
-    }
-    void deserialize(OutputArchive &archive,const char *key) {
-        if(children.empty()) {
-            archive(name,content);
-        } else {
-            archive(name,children);
-        }
-    }
-};
-
 void serialize(OutputArchive &archive,const char *key,const char *string);
 void deserialize(OutputArchive &archive,const char *key,const char *string);
 void serialize(OutputArchive &archive,const char *key,int number);
@@ -115,26 +83,46 @@ public:
     }
 };
 
-class Settings : public SettingItem<int> {
+class SettingItem {
 public:
-    Settings(std::vector<SettingItem*> items) : SettingItem<int>("Settings",items){}
+    const char *name;
+    std::vector<int> children;
+    SettingItem(const char *name,std::vector<int> children){
+        this->name = name;
+        this->children = children;
+    }
+    void serialize(OutputArchive &archive,const char *key) {
+        if(children.empty()) {
+            Serial.printf("SettingItem#serialize(%s,content)",name);
+            // archive(name,getContent());
+        } else {
+            Serial.printf("SettingItem#serialize(%s,children)",name);
+            archive(name,children);
+        }
+    }
+    void deserialize(OutputArchive &archive,const char *key) {
+        if(children.empty()) {
+            // archive(name,getContent());
+        } else {
+            archive(name,children);
+        }
+    }
+};
+
+class Settings : public SettingItem {
+public:
+    Settings(std::vector<int> items) : SettingItem("Settings",items){}
     void load(String path){
     }
     void save(String path = jsonFilePath){
         OutputArchive archive = OutputArchive();
         std::vector<int> vec = std::vector<int>{1,2,3,4};
-        // archive("test","tanosiine");
+        archive("test","tanosiine");
         archive(name,vec);
-        //archive(name,*this);
+        archive(name,*this);
         char output[maxJsonFileSize];
         archive.toJSON(output);
         Serial.println(output);
-        // DynamicJsonDocument doc(maxJsonCapacity);
-        // doc["value"] = "value";
-        // has_serialize<const char*> hoge = "value";
-        // // Serialize
-        // char output[maxJsonFileSize];
-        // serializeJson(doc,output);
         // File file = SD.open(path,FILE_WRITE);
         // file.print(output);
         // file.close();
